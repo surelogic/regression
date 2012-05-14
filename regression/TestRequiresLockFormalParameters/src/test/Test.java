7901cfcf5e25d762ff09023c0b28621abccc3030
Aaron Greenhouse <aarong@surelogic.com>
2012-05-14 14:23:15 -0400

    New Test for checking whether a formal paramter named in a RequiresLock annotation is effectively final.
    Former-commit-id: 8c7f2b570fc8b0b9d8cc062227acdbb7346351c5

---
+++ regression/TestRequiresLockFormalParameters/src/test/Test.java
@@ -0,0 +1,29 @@
+package test;
+
+import com.surelogic.RegionLock;
+import com.surelogic.RequiresLock;
+
+public class Test {
+  @RequiresLock("other:Lock")
+  public void method1(final Other other) {
+    // Okay, other is final, nothing to check
+  }
+
+  @RequiresLock("other:Lock")
+  public void method2(Other other) {
+    // Okay, other is read, but not modified
+    // Issue a warning to make the parameter final
+    Object o = other;
+  }
+
+  @RequiresLock("other:Lock")
+  public void method3(Other other) {
+    // BAD: other is modified
+    other = new Other();
+  }
+}
+
+@RegionLock("Lock is this protects Instance")
+class Other {
+  
+}
