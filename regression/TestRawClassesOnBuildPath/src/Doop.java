20e24b15e2b8b46dc317a65b7e76a054c2ffeb33
edwin <edwin.chan@surelogic.com>
2012-10-03 14:32:49 -0400

    Added TestRawClassesOnBuildPath
    Former-commit-id: 74addee4bcc8b2eba0051a0e5dd0f07d98a9167b

---
+++ regression/TestRawClassesOnBuildPath/src/Doop.java
@@ -0,0 +1,13 @@
+import java.lang.String;
+import pkg.boop.BoopPkg;
+
+import com.surelogic.Starts;
+
+public class Doop {
+
+	@Starts("nothing")
+	public static void main(String[] args) {
+		Boop.boop();
+		BoopPkg.boop();
+	}
+}
