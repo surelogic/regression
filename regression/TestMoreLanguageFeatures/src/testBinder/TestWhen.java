ab2df305c1092adf9131887f3ed634cf280a592f
Aaron Greenhouse <aarong@surelogic.com>
2015-06-09 14:22:27 -0400

    Started to update reg tests to only use the classic uniqueness analysis, not the unique + from analysis.
    Also removed lots of old oracles
    
    
    Former-commit-id: d28b99c53169efbf3b222561eee7c5ee3fade648

--- regression/TestMoreLanguageFeatures/src/testBinder/TestWhen.java
+++ regression/TestMoreLanguageFeatures/src/testBinder/TestWhen.java
@@ -1,8 +1,11 @@
 package testBinder;
 
 import com.surelogic.Containable;
+import com.surelogic.Unique;
 
 @Containable(whenContainable={"A", "D"})
 public class TestWhen<A, B, C> {
+
+	@Unique("return") public TestWhen(){}
 
 }
